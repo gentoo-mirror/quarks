0550745fdeecccd6bddd93f2d8c3d620af338442
Stefan Reimer <it@startux.de>
2013-09-02 17:54:59 -0700

    scripts + conf updates and merge

--- scripts/update_box.sh
+++ scripts/update_box.sh
@@ -2,21 +2,23 @@
 # set -x
 CPUS=$(nproc)
 
-emerge --update --deep --newuse --pretend --jobs ${CPUS} world
-
-echo "Press Ctrl-C to abort..."
-read
+# as many cc as CPUs
+export MAKEOPTS="-j${CPUS}"
 
 # Trigger mount
 # ls /mnt/portage/distfiles /mnt/portage/portage > /dev/null
 
-emerge --update --deep --newuse --keep-going --jobs ${CPUS} world
+# Half as many emerge as CPUs, to speed up configure runs
+emerge --ask --update --deep --newuse --keep-going --accept-properties=-interactive --jobs $((CPUS/2)) world
 
 # echo "Fixing pax flags..."
 # ${SCRIPT_DIR}/fix_grsec.sh
 
 echo "Going to remove unneeded packages ..."
-emerge --depclean --jobs ${CPUS}
+emerge --depclean
 
 echo "Fixing dependencies..."
 revdep-rebuild -i
+
+echo "Updating eix cache..."
+eix-update -q
