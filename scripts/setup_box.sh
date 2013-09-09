49f2a2acd61bdadb8d34c8857e466036bf1fc744
Stefan Reimer <it@startux.de>
2013-09-08 21:34:08 -0700

    Add conf to setup script

--- scripts/setup_box.sh
+++ scripts/setup_box.sh
@@ -3,12 +3,17 @@ set -x
 
 # Sets up all kinds of softlinks and scripts to share all gentoo/portage 
 # across multiple machines backed by git repository
-SBIN_PREFIX="/usr/local/sbin"
-SBIN_FROM="update_portage.sh update_box.sh make_kernel.sh"
-SBIN_TO="/mnt/portage/overlays/quarks/scripts"
 
-pushd $SBIN_PREFIX > /dev/null
-for i in $SBIN_FROM; do
-	[ -e $i ] || ln -s $SBIN_TO/$i $i
-done
-popd > /dev/null
+link() {
+	FROM=$1
+	TO=$2
+	PREFIX=$3
+	pushd $PREFIX > /dev/null
+	for i in $FROM; do
+		[ -e $i ] || ln -s $TO/$i $i
+	done
+	popd > /dev/null
+}
+
+link "update_portage.sh update_box.sh make_kernel.sh" "/mnt/portage/overlays/quarks/scripts" "/usr/local/sbin"
+link "make.conf package.use package.keywords package.license" "/mnt/portage/overlays/quarks/conf/portage" "/etc/portage"
